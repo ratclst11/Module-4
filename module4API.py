bf070d7e35c50025ac04b6924aa2ed703d7b1f82
ratclst11 <sratcliff11@ivytech.edu>
2025-02-08 16:08:07 -0500

    Add files via upload

---
+++ module4API.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_name = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    publisher = db.Column(db.String(100), nullable=False)
+
+@app.route('/books', methods=['GET'])
+def get_books():
+    books = Book.query.all()
+    return jsonify([{'id': book.id, 'book_name': book.book_name, 'author': book.author, 'publisher': book.publisher} for book in books])
+
+@app.route('/books/<int:id>', methods=['GET'])
+def get_book(id):
+    book = Book.query.get_or_404(id)
+    return jsonify({'id': book.id, 'book_name': book.book_name, 'author': book.author, 'publisher': book.publisher})
+
+@app.route('/books', methods=['POST'])
+def add_book():
+    data = request.get_json()
+    new_book = Book(book_name=data['book_name'], author=data['author'], publisher=data['publisher'])
+    db.session.add(new_book)
+    db.session.commit()
+    return jsonify({'id': new_book.id, 'book_name': new_book.book_name, 'author': new_book.author, 'publisher': new_book.publisher}), 201
+
+@app.route('/books/<int:id>', methods=['PUT'])
+def update_book(id):
+    data = request.get_json()
+    book = Book.query.get_or_404(id)
+    book.book_name = data['book_name']
+    book.author = data['author']
+    book.publisher = data['publisher']
+    db.session.commit()
+    return jsonify({'id': book.id, 'book_name': book.book_name, 'author': book.author, 'publisher': book.publisher})
+
+@app.route('/books/<int:id>', methods=['DELETE'])
+def delete_book(id):
+    book = Book.query.get_or_404(id)
+    db.session.delete(book)
+    db.session.commit()
+    return '', 204
+
+if __name__ == '__main__':
+    app.run(debug=True)
